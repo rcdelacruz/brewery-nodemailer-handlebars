e95a010c13c9dba1e679fb4d33814e0e9c60fa1f
Ronald DC <apps@thebrewery.dev>
2021-07-08 13:13:41 +0800

    first commit

--- index.js
+++ index.js
@@ -2,62 +2,65 @@ require("dotenv").config();
 
 const nodemailer = require("nodemailer");
 const hbs = require("nodemailer-express-handlebars");
+const path = require("path");
 const log = console.log;
-
 
 const sendMail = (mailOptions, engineOptions, SMTPCredentials) => {
   let credentials = {};
+  log(process.env);
   if (SMTPCredentials === undefined) {
     credentials = {
       port: process.env.SMTP_PORT,
       host: process.env.SMTP_HOST,
       secure: true,
       auth: {
-          user: process.env.SMTP_USERNAME,
-          pass: process.env.SMTP_PASSWORD,
+        user: process.env.SMTP_USERNAME,
+        pass: process.env.SMTP_PASSWORD,
       },
-      debug: process.env.IS_DEBUG
-    }
+      debug: process.env.IS_DEBUG,
+    };
   } else {
     credentials = {
       port: SMTPCredentials.port,
       host: SMTPCredentials.host,
       secure: SMTPCredentials.secure,
       auth: {
-          user: SMTPCredentials.auth.user,
-          pass: SMTPCredentials.auth.pass,
+        user: SMTPCredentials.auth.user,
+        pass: SMTPCredentials.auth.pass,
       },
-      debug: SMTPCredentials.is_debug
-    }
+      debug: SMTPCredentials.is_debug,
+    };
   }
   let transporter = nodemailer.createTransport(credentials);
-  
 
   transporter.use(
     "compile",
     hbs({
-        viewEngine: {
-          extName: ".hbs" || engineOptions.extName,
-          partialsDir: "./templates" || engineOptions.templatePath,
-          layoutsDir: "./templates" || engineOptions.templatePath,
-          defaultLayout: "",
-        },
-        viewPath: "./templates" || engineOptions.templatePath,
+      viewEngine: {
         extName: ".hbs" || engineOptions.extName,
+        partialsDir:
+          path.resolve(__dirname, "./templates") || engineOptions.templatePath,
+        layoutsDir:
+          path.resolve(__dirname, "./templates") || engineOptions.templatePath,
+        defaultLayout: "",
+      },
+      viewPath:
+        path.resolve(__dirname, "./templates") || engineOptions.templatePath,
+      extName: ".hbs" || engineOptions.extName,
     })
   );
 
   transporter.sendMail(mailOptions, (err) => {
     if (err) {
-      return log({ 
+      return log({
         "nodemailer-error": err,
-        "message": "Error occurs"
+        message: "Error occurs",
       });
     }
-    return log({"nodemailer-success": "Email sent!"});
+    return log({ "nodemailer-success": "Email sent!" });
   });
-}
+};
 
 module.exports = {
-    sendMail
-}
+  sendMail,
+};
